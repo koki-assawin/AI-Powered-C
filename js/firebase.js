80e8137337480e837bd7b99ef52e180fd3a9082c
Assawin Chullamool <koki.assawin@gmail.com>
2026-05-11 10:04:03 +0700

    fix: remove firebase-functions SDK init that was crashing the app on load

--- js/firebase.js
+++ js/firebase.js
@@ -20,10 +20,9 @@ if (!firebase.apps.length) {
 }
 
 // Export service instances as globals (accessible by all subsequent scripts)
-const db        = firebase.firestore();
-const auth      = firebase.auth();
-const rtdb      = firebase.database();
-const functions = firebase.functions('asia-southeast1');
+const db   = firebase.firestore();
+const auth = firebase.auth();
+const rtdb = firebase.database();
 
 // Google Auth Provider
 const googleProvider = new firebase.auth.GoogleAuthProvider();
